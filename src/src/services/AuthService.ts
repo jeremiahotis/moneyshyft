14ca0486ee7e1d830e0b7d38bbe7febf6ba5550f
Jeremiah Otis <jeremiah@jeremiahotis.com>
2025-12-30 06:17:37 -0500

    Replace bcrypt with bcryptjs
    - Update AuthService to use bcryptjs instead of bcrypt
    - Replace bcrypt dependency with bcryptjs in package.json
    - Replace @types/bcrypt with @types/bcryptjs
    
    bcryptjs is a pure JavaScript implementation that doesn't require
    native compilation, making it easier to deploy across platforms

--- src/src/services/AuthService.ts
+++ src/src/services/AuthService.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import db from '../config/knex';
 import { generateAccessToken, generateRefreshToken, JWTPayload } from '../utils/jwt';
 import { generateInvitationCode } from '../utils/invitationCode';
