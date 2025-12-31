1b5add58e0237dcea82f766b640f1a3b106b5a47
Jeremiah Otis <jeremiah@jeremiahotis.com>
2025-12-31 07:39:59 -0500

    Add plain JavaScript knexfile.js for production migrations
    - Create src/knexfile.js with CommonJS module.exports for Knex compatibility
    - Update production migration scripts to use knexfile.js instead of compiled dist/knexfile.js
    - This avoids TypeScript export compilation issues in production Docker containers
    
    Note: Production Dockerfile needs to copy knexfile.js (add: COPY knexfile.js ./)

--- src/knexfile.js
+++ src/knexfile.js
@@ -1,51 +1,47 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+require('dotenv').config();
+
 const config = {
-    development: {
-        client: 'postgresql',
-        connection: process.env.DATABASE_URL || {
-            host: 'postgres',
-            port: 5432,
-            database: 'moneyshyft',
-            user: 'jeremiahotis',
-            password: 'Oiruueu12'
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            tableName: 'knex_migrations',
-            directory: './src/migrations',
-            extension: 'ts'
-        },
-        seeds: {
-            directory: './src/seeds/dev',
-            extension: 'ts'
-        }
+  development: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL || {
+      host: 'localhost',
+      port: 5432,
+      database: 'moneyshyft',
+      user: 'jeremiahotis',
+      password: 'Oiurueu12'
     },
-    production: {
-        client: 'postgresql',
-        connection: process.env.DATABASE_URL,
-        pool: {
-            min: 2,
-            max: 20
-        },
-        migrations: {
-            tableName: 'knex_migrations',
-            directory: './src/migrations',
-            extension: 'ts'
-        },
-        seeds: {
-            directory: './src/seeds/production',
-            extension: 'ts'
-        }
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './src/migrations',
+      extension: 'ts'
+    },
+    seeds: {
+      directory: './src/seeds/dev',
+      extension: 'ts'
     }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 20
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './dist/migrations',
+      extension: 'js'  // Use compiled JavaScript files in production
+    },
+    seeds: {
+      directory: './dist/seeds/production',
+      extension: 'js'  // Use compiled JavaScript files in production
+    }
+  }
 };
-exports.default = config;
-//# sourceMappingURL=knexfile.js.map
+
+module.exports = config;
